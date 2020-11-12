cfb0f194cca15a90e13fe7bbb73f6497a40e4755
Ahsan hyder <39864885+ahsanhyder625@users.noreply.github.com>
2020-11-12 23:08:43 +0530

    Upadted and Live Digial Clock

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './index.css';
 
 function App() {
 
-const [time, settime] = useState('00:00:00')
+let time = new Date().toLocaleTimeString()
+const [ctime, settime] = useState(time)
 
-const handleClick = () =>{
-  let date = new Date()
-  settime(date.toLocaleTimeString())
+const UpdateTime= () =>{
+  time = new Date().toLocaleTimeString()
+  settime(time)
 }
 
+setInterval(UpdateTime,1000)
   return (
     <div className="App">
-      <h1>{time}</h1>
-      <button onClick={handleClick}>Get Time</button>
+      <h1>{ctime}</h1>
+      {/* <button onClick={handleClick}>Get Time</button> */}
     </div>
   );
 }
